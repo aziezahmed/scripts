cc6fe1fb48610443ef224e91770a30378ce8814c
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2020-12-04 11:35:22 +0000

    indentation

--- feed.py
+++ feed.py
@@ -1,13 +1,13 @@
 import feedparser
 
 def main():
-	feed = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
-	for post in feed.entries:
-    	print((
-       		f"{post.title}\n"
-        	f"{post.summary}\n"
-        	#f"{post.link}\n"
-    	))
+    feed = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
+    for post in feed.entries:
+        print((
+            f"{post.title}\n"
+            f"{post.summary}\n"
+            #f"{post.link}\n"
+        ))
 
 if __name__ == "__main__":
     main()

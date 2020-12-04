d3639fde71b7fd41fe786ceba6ef43c5b974caab
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2020-12-04 11:32:36 +0000

    add main method

--- feed.py
+++ feed.py
@@ -1,8 +1,13 @@
 import feedparser
-feed = feedparser.parse("RSS_FEED_URL")
-for post in feed.entries:
-    print((
-        f"{post.title}\n"
-        f"{post.summary}\n"
-        #f"{post.link}\n"
-    ))
+
+def main():
+	feed = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
+	for post in feed.entries:
+    	print((
+       		f"{post.title}\n"
+        	f"{post.summary}\n"
+        	#f"{post.link}\n"
+    	))
+
+if __name__ == "__main__":
+    main()

bf6ce489bf19fee2c8c3a3ef75163ba532d5d73e
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2020-12-04 11:07:05 +0000

    Add weather for next three hours and tomorrows forecast

--- weather.py
+++ weather.py
@@ -1,5 +1,24 @@
 import pyowm
 
+def print_weather(weather):
+    temperature = weather.temperature("celsius").get("temp")
+    feels_like = weather.temperature("celsius").get("feels_like")
+
+    if(not temperature):
+        temperature = weather.temperature("celsius").get("day")
+        feels_like = weather.temperature("celsius").get("feels_like_day")
+    status = weather.status
+    detailed_status = weather.detailed_status
+    
+    print((f"Weather for {weather.reference_time('iso')}"))
+    print((
+        f"Temperature: {temperature}\n"
+        f"Feels Like: {feels_like}\n"
+        f"{status}\n"
+        f"{detailed_status}\n"
+    ))
+    
+
 def main():
     lat = 51.5
     lon = -0.116
@@ -8,23 +27,18 @@ def main():
     mgr = owm.weather_manager()
 
     one_call = mgr.one_call(lat=lat, lon=lon)
-    
-    current_temperature = one_call.current.temperature("celsius")
+    print_weather(one_call.current);
 
-    temperature = current_temperature.get("temp")
-    feels_like = current_temperature.get("feels_like")
-    status = one_call.current.status
-    detailed_status = one_call.current.detailed_status
-
-    print((
-        f"Current Temperature: {temperature}\n"
-        f"Feels Like: {feels_like}\n"
-        f"{status}\n"
-        f"{detailed_status}\n"
-    ))
+    forecast_hourly = one_call.forecast_hourly
+    print_weather(forecast_hourly[1])
+    print_weather(forecast_hourly[3])
+    print_weather(forecast_hourly[5])
 
     forecast_daily = one_call.forecast_daily
-    
+    tomorrows_forecast = forecast_daily[1]
+    print_weather(tomorrows_forecast)
+
+
 if __name__ == "__main__":
     main()
 
